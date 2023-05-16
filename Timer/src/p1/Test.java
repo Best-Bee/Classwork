a3cea41562260c55f99cb36ebf915bf7a193d1c2
Brendan Best <131709755+Best-Brendan@users.noreply.github.com>
2023-05-16 10:42:29 -0400

    Added Timer Example

---
+++ Timer/src/p1/Test.java
@@ -0,0 +1,11 @@
+package p1;
+public class Test{
+	public static void main(String[] args) {
+		int MAX = 10;
+		System.out.println("This is a test");
+		for (int i = 1 ; i <= MAX; i++) {
+			System.out.println("... " + i);
+		}
+
+	}
+}
